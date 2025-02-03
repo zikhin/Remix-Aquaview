93f60b0e99d541ce0f59271c9e402ea8e1fe1037
Mohammed Yaseen M <akbarmayama@gmail.com>
2025-02-03 22:48:03 +0530

    removed width and height and responsive

--- app/components/cards/homeCards.jsx
+++ app/components/cards/homeCards.jsx
@@ -1,9 +1,9 @@
 export default function HomeCards(props){
     return(
         <>
-        <div className="w-[560px] h-[480px] flex flex-col p-3 justify-center">
-        <img className=" shadow-sm w-[520px] h-[300px] rounded-lg pb-5 px-5 " src={props.imgSrc} alt={props.imgAtl}/>
-        <div className="w-full h-[115] px-7 pb-10 mb-3">
+        <div className="flex flex-col w-full lg:h-auto md:h-[40%] sm:h-[40%] ">
+        <img className="lg:w-[90%] shadow-md lg:h-[80%] h-[60%] w-[80%] mx-auto  object-fill rounded-lg" src={props.imgSrc} alt={props.imgAtl}/>
+        <div className="w-full lg:h-[40%] h-[20%] lg:px-7 lg:pb-10 lg:mb-3">
         <h4 className="text-center h-6 mb-3 text-[#1f2937] font-semibold text-lg">{props.title}</h4>
         <p className="text-center text-[#4b5563 ] font-normal text-base" >{props.description}</p>
         </div>
@@ -11,3 +11,4 @@ export default function HomeCards(props){
         </>
         )
     }
+
--- app/routes/home.jsx
+++ app/routes/home.jsx
@@ -6,11 +6,11 @@ import residential from "../images/home-aquarium.jpg"
 export default  function Home(){
     return (
         <>
-        <section className="w-[1140px] min-h-svh mx-auto mb-8 flex flex-col justify-center items-center">
+        <section className="lg:w-[90%] min-w-fit min-h-svh mx-auto mb-8 flex flex-col justify-center items-center">
         <h2 className="h-12 font-sans font-bold text-4xl text-[#1f2937]">CUSTOM AQUARIUM</h2>
         <div className="text-center w-24 py-2 border-b-2 border-red-700 flex-col lg:flex-row "></div>
-        <section className="flex  justify-center">
-        <div className="w-full h-72 flex lg:flex-row justify-between p-3" >
+        <section className="flex mt-4 justify-center lg:w-[80%] lg:h-80 h-[100vh] gap-4 lg:gap-4 flex-col lg:flex-row p-3 columns-2
+        border-2 border-black">
         <HomeCards 
         title="RESIDENTIAL AQUARIUM" 
         description="Residential Aquarium: Indoor with Standard Sizes Range from 2 1/2 to 250 gallons" 
@@ -21,6 +21,10 @@ export default  function Home(){
             description="Commercial Aquarium: Office Aquarium,Hotel Aquarium,Hospital Aquarium" 
             imgSrc={commercial} 
             imgAtl="COMMERCIAL AQUARIUM"/>
+        </section>
+        <section className="relative bg-[url(../../public/homepage-background.jpg)] -[600px] h-[300px] border-2">
+        <div className="absolute">
+        <p>Hello inner text</p>
         </div>
         </section>
         </section>
